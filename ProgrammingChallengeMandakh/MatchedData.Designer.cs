edf459e0a33f8f5ffea1e1cc61f8fb87666b4006
Mandaa109 <mandaa0109@gmail.com>
2019-03-22 11:13:29 -0500

    InitialCommit

---
+++ ProgrammingChallengeMandakh/MatchedData.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ProgrammingChallengeMandakh
+{
+    partial class MatchedData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOurData = new System.Windows.Forms.Button();
+            this.textBox_ourDataPath = new System.Windows.Forms.TextBox();
+            this.btnTheirData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCombine = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnOurData
+            // 
+            this.btnOurData.Location = new System.Drawing.Point(45, 40);
+            this.btnOurData.Name = "btnOurData";
+            this.btnOurData.Size = new System.Drawing.Size(150, 56);
+            this.btnOurData.TabIndex = 0;
+            this.btnOurData.Text = "Choose Our data";
+            this.btnOurData.UseVisualStyleBackColor = true;
+            this.btnOurData.Click += new System.EventHandler(this.ourDataBtn_Click);
+            // 
+            // textBox_ourDataPath
+            // 
+            this.textBox_ourDataPath.Location = new System.Drawing.Point(210, 57);
+            this.textBox_ourDataPath.Name = "textBox_ourDataPath";
+            this.textBox_ourDataPath.ReadOnly = true;
+            this.textBox_ourDataPath.Size = new System.Drawing.Size(478, 22);
+            this.textBox_ourDataPath.TabIndex = 1;
+            // 
+            // btnTheirData
+            // 
+            this.btnTheirData.Location = new System.Drawing.Point(45, 110);
+            this.btnTheirData.Name = "btnTheirData";
+            this.btnTheirData.Size = new System.Drawing.Size(150, 49);
+            this.btnTheirData.TabIndex = 3;
+            this.btnTheirData.Text = "Choose Their data";
+            this.btnTheirData.UseVisualStyleBackColor = true;
+            this.btnTheirData.Click += new System.EventHandler(this.btnTheirData_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(478, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnCombine
+            // 
+            this.btnCombine.Location = new System.Drawing.Point(45, 176);
+            this.btnCombine.Name = "btnCombine";
+            this.btnCombine.Size = new System.Drawing.Size(150, 52);
+            this.btnCombine.TabIndex = 6;
+            this.btnCombine.Text = "Combine data and Export";
+            this.btnCombine.UseVisualStyleBackColor = true;
+            this.btnCombine.Click += new System.EventHandler(this.btnCombine_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(210, 189);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(478, 23);
+            this.progressBar.TabIndex = 10;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(207, 236);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(110, 17);
+            this.lblStatus.TabIndex = 11;
+            this.lblStatus.Text = "Processing...0%";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 286);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnCombine);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTheirData);
+            this.Controls.Add(this.textBox_ourDataPath);
+            this.Controls.Add(this.btnOurData);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Liquidus Back-End programming Challenge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOurData;
+        private System.Windows.Forms.TextBox textBox_ourDataPath;
+        private System.Windows.Forms.Button btnTheirData;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCombine;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ ProgrammingChallengeMandakh/MatchedData.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProgrammingChallengeMandakh
+{
+    public partial class MatchedData : Form
+    {
+        #region [FormLoad]
+        public MatchedData()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region [Declaration - Variable]
+        OpenFileDialog ofd = new OpenFileDialog();
+        DataTable dtOur;
+        DataTable dtTheir;
+        DataParameter _inputParameter;
+        struct DataParameter
+        {
+            public string FileName { get; set; }
+        }
+        public bool isSuccessfullySaved { get; set; } = false;
+        #endregion
+
+        #region [Events - Button]
+        private void ourDataBtn_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox_ourDataPath.Text = ofd.FileName;
+                BindDataCSVOurData(this.textBox_ourDataPath.Text);
+            }
+        }
+
+        private void btnTheirData_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = ofd.FileName;
+                BindDataCSVTheirData(this.textBox1.Text);
+            }
+        }
+
+        private void btnCombine_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+                return;
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "CSV|*.csv", ValidateNames = true })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    _inputParameter.FileName = sfd.FileName;
+                    progressBar.Minimum = 0;
+                    progressBar.Value = 0;
+                    backgroundWorker.RunWorkerAsync(_inputParameter);
+                }
+            }
+        }
+        #endregion
+
+        #region [Custom defined methods]
+        private void BindDataCSVOurData(string filePath)
+        {
+            dtOur = new DataTable();
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(filePath);
+                if (lines.Length > 0)
+                {
+                    string firstLine = lines[0];
+                    string[] headerLabels = firstLine.Split(',');
+
+                    foreach (string headerWord in headerLabels)
+                    {
+                        dtOur.Columns.Add(new DataColumn(headerWord));
+                    }
+
+                    for (int r = 1; r < lines.Length; r++)
+                    {
+                        string[] dataWords = lines[r].Split(',');
+                        DataRow dr = dtOur.NewRow();
+                        int columnIndex = 0;
+                        foreach (string headerWord in headerLabels)
+                        {
+                            dr[headerWord] = dataWords[columnIndex++];
+                        }
+                        dtOur.Rows.Add(dr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+            }
+            
+        }
+
+        private void BindDataCSVTheirData(string filePath)
+        {
+            dtTheir = new DataTable();
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(filePath);
+                if (lines.Length > 0)
+                {
+                    string firstLine = lines[0];
+                    string[] headerLabels = firstLine.Split(',');
+
+                    foreach (string headerWord in headerLabels)
+                    {
+                        dtTheir.Columns.Add(new DataColumn(headerWord));
+                    }
+
+                    dtTheir.Columns.Add(new DataColumn("TrackingID"));
+
+                    for (int r = 1; r < lines.Length; r++)
+                    {
+                        string[] dataWords = lines[r].Split(',');
+                        DataRow dr = dtTheir.NewRow();
+                        int columnIndex = 0;
+                        foreach (string headerWord in headerLabels)
+                        {
+                            dr[headerWord] = dataWords[columnIndex++];
+                            if (headerWord.ToUpper() == "CREATIVE")
+                            {
+                                Regex regex = new Regex(@"_BL");
+                                Match match = regex.Match(dataWords[columnIndex - 1]);
+                                if (match.Success)
+                                {
+                                    if (dr["Creative"].ToString().Length > 20)
+                                    {
+                                        string str = dr["Creative"].ToString()
+                                        .Substring(dr["Creative"].ToString().Length - 20);
+                                        dr["TrackingId"] = str.Substring(str.IndexOf("_BL"));
+                                    }
+                                }
+                            }
+                        }
+                        dtTheir.Rows.Add(dr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+            }
+           
+        }
+
+        #endregion
+
+        #region [Events - BackgroundWorker]
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            isSuccessfullySaved = false;
+            try
+            {
+                // I put Our data and Their data into 2 datatables and now I use LINQ to join tables
+                // and variable results contain matched data
+                var results = from table1 in dtTheir.AsEnumerable()
+                              join table2 in dtOur.AsEnumerable() on table1["TrackingID"] equals table2["TrackingID"]
+                              select new
+                              {
+                                  ID = table2[0],
+                                  Name = table2[1],
+                                  TrackingID = table2[2],
+                                  Creative = table1[0],
+                                  Clicks = table1[1],
+                                  Impressions = table1[2],
+                                  DateStamp = table1[3],
+                              };
+
+
+                //This next section retrieve all data from results table and export to CSV file
+                string fileName = ((DataParameter)e.Argument).FileName;
+                int index = 1;
+                int process = results.Count();
+                using (StreamWriter sw = new StreamWriter(new FileStream(fileName, FileMode.Create), Encoding.UTF8))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine(String.Format("{0},{1},{2},{3},{4},{5},{6}",
+                        "ID", "Name", "Tracking ID", "Creative", "Clicks", "Impressions", "DateStamp"));
+                    if (!backgroundWorker.CancellationPending)
+                    {
+                        foreach (var item in results)
+                        {
+                            backgroundWorker.ReportProgress(index++ * 100 / process);
+                            sb.AppendLine(String.Format("{0},{1},{2},{3},{4},{5},{6}",
+                                item.ID, item.Name, item.TrackingID, item.Creative, item.Clicks, item.Impressions, item.DateStamp));
+                        }
+                    }
+                    sw.Write(sb.ToString());
+                    isSuccessfullySaved = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);   
+            }
+           
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblStatus.Text = string.Format("Processing...{0}%", e.ProgressPercentage);
+            progressBar.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            Thread.Sleep(100);
+            if (isSuccessfullySaved)
+                lblStatus.Text = "Your data has been successfully exported!";
+            else
+                lblStatus.Text = "Please check your files and try again!";
+        }
+        #endregion
+    }
+}
